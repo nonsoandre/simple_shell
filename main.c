d554a05f8c4e50f980c5d9581502d681fd288b74
Felicity Essien <felicityabel2016@gmail.com>
2023-11-14 21:32:21 -0800

    fixed betty errors

--- main.c
+++ main.c
@@ -1,48 +1,48 @@
 #include "main.h"
 
 extern char **environ;
+/**
+ * main - the entry point to the shell
+ * Return: returns 0 if success
+ */
 int main(void)
 {
-    char *lineptr = NULL;
-    size_t n = 0;
-
-    while (1) {
-        printf("$ ");
-        ssize_t num_of_chars_read = getline(&lineptr, &n, stdin);
-        if (num_of_chars_read == -1) {
-            printf("Exiting shell... \n");
-            free(lineptr);
-            return (-1);
-        }
-
-        // Call the tokenization function to handle tokenization
-        char *argv[MAX_ARGUMENTS];
-        int num_tokens = tokenize_line(lineptr, " \n", argv);
-
-        if (num_tokens > 0) {
-            if (strcmp(argv[0], "exit") == 0) {
-                printf("Exiting shell... \n");
-                free(lineptr);
-                return (0);
-            }
-            else if (strcmp(argv[0], "env") == 0) {
-                // Print the current environment
-                char **env = environ;
-                while (*env != NULL) {
-                    printf("%s\n", *env);
-                    env++;
-                }
-            }
-            else {
-                execomd(argv);
-            }
-        }
-
-        // Free memory allocated by getline
-        free(lineptr);
-        lineptr = NULL;
-    }
-
-    return (0);
+char *lineptr = NULL;
+size_t n = 0;
+char **env = environ;
+while (1)
+{
+printf("$ ");
+ssize_t num_of_chars_read = getline(&lineptr, &n, stdin);
+if (num_of_chars_read == -1)
+{
+printf("Exiting shell... \n");
+free(lineptr);
+return (-1); }
+char *argv[MAX_ARGUMENTS];
+int num_tokens = tokenize_line(lineptr, " \n", argv);
+if (num_tokens > 0)
+{
+if (strcmp(argv[0], "exit") == 0)
+{
+printf("Exiting shell... \n");
+free(lineptr);
+return (0);
 }
-
+else if (strcmp(argv[0], "env") == 0)
+{
+//char **env = environ;
+while (*env != NULL)
+{
+printf("%s\n", *env);
+env++; }
+}
+else
+{
+execomd(argv);
+} }
+free(lineptr);
+lineptr = NULL;
+}
+return (0);
+}
--- tokenizer.c
+++ tokenizer.c
@@ -1,21 +1,30 @@
 #include "main.h"
-int tokenize_line(char *line, const char *delim, char **argv) {
-    char *token;
-    int num_of_tokens = 0;
+/**
+ * tokenize_line - a function to split command line arguments
+ * @line: the command line
+ * @delim: the delimeter
+ * @argv: the array of command line arguments
+ * Return: returns 0 on success
+ */
+int tokenize_line(char *line, const char *delim, char **argv)
+{
+char *token;
+int num_of_tokens = 0;
 
-    token = strtok(line, delim);
-    while (token != NULL) {
-        num_of_tokens++;
-        argv[num_of_tokens - 1] = malloc(strlen(token) + 1);
-        if (argv[num_of_tokens - 1] == NULL) {
-            perror("tsh: failed to allocate memory");
-            exit(EXIT_FAILURE);
-        }
-        strcpy(argv[num_of_tokens - 1], token);
-        token = strtok(NULL, delim);
-    }
-    argv[num_of_tokens] = NULL;
-
-    return num_of_tokens;
+token = strtok(line, delim);
+while (token != NULL)
+{
+num_of_tokens++;
+argv[num_of_tokens - 1] = malloc(strlen(token) + 1);
+if (argv[num_of_tokens - 1] == NULL)
+{
+perror("tsh: failed to allocate memory");
+exit(EXIT_FAILURE);
+}
+strcpy(argv[num_of_tokens - 1], token);
+token = strtok(NULL, delim);
+}
+argv[num_of_tokens] = NULL;
+return (num_of_tokens);
 }
 
